a3bbf6773ddc26ece9cd221b07b0dcbf8f21d0c9
Miha Avsec <mihavsec@gmail.com>
2019-11-15 11:57:58 +0100

    dodan napad na anomalne krivulje

---
+++ magistersko delo/anomalne.py
@@ -0,0 +1,51 @@
+from base import *
+import random
+from fractions import Fraction
+
+def Lift(P,Q,meja = 20):
+    p = P.mod  
+    x1 = P.x + random.randint(0,meja)*p
+    x2 = Q.x + random.randint(0,meja)*p
+    if P.x != Q.x:
+        y1 = P.y + random.randint(0,meja)*p
+        razl = abs(x2-x1)
+        i=0
+        while True:
+            y2 = Q.y + i*p
+            if pow(y2,2,razl) == pow(y1,2,razl):
+                break
+            i += 1
+        A1 = int((y2**2-y1**2)/(x2-x1)-(x2**3-x1**3)/(x2-x1))
+        B1 = y1**2-x1**3-A1*x1
+    else:
+        x2 = x1
+        y1 = P.y + random.randint(0,meja)*p
+        A1 = A + random.randint(0,meja)*p
+        B1 = y1**2-x1**3-A1*x1
+        if P.y == Q.y:
+            y2 = y1
+        else:
+            y2 = -y1
+
+    return(Point(A1,B1,p**2,x1,y1),Point(A1,B1,p**2,x2,y2))
+
+def AnomalneAttack(P,Q):
+    (P1,Q1) = Lift(P,Q)
+    p = P.mod
+    P2 = (p-1)*P1
+    Q2 = (p-1)*Q1
+    m1 = p*Fraction(P2.y-P1.y,P2.x-P1.x)
+    m2 = p*Fraction(Q2.y-Q1.y,Q2.x-Q1.x)
+
+    k = m1/m2
+    k1 = k.numerator
+    k2 = k.denominator
+    k2 = NumberMod(k2,p).inverse().num
+    k = (k1*k2) % p
+    return k
+    
+            
+    
+P = Point(108,4,853,0,2)
+Q= Point(108,4,853,563,755)
+k = AnomalneAttack(P,Q)
