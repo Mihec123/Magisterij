343f0cc5310341464e960e4a8392ee50829b8c3e
Miha Avsec <mihavsec@gmail.com>
2019-12-02 11:38:35 +0100

    prvi spell check pa dodana nova koda v latex
    Zamenjani datoteki MOV,IndexCalculus za stare glej repozitorij za nazaj

--- magistersko delo/IndexCalculus.py
+++ magistersko delo/IndexCalculus.py
@@ -1,249 +1,81 @@
-import mip
-import math
-import random
-import numpy as np
-from base import *
-
-
-def PrimeSearch(num,mod,base):
-    """
-    Opis:
-       PrimeSearch  vrne  razcep na prafaktorje iz baze ce
-       tak razcep obstaja, v nasprotnem primeru vrne None.
-
-     Definicija:
-       PrimeSearch(num,mod,base)
-
-     Vhodni podatki:
-       num...stevilo, ki ga hocemo razcepiti na prafaktorje
-       mod...modul s katerim delamo
-       base...baza prastevil s katerimi delamo
-
-     Izhodni  podatek:
-       seznam_prafaktorjev ,kjer prvi element pomeni
-       ali je spredaj - ali + (ce je element 0
-       potem je +, ce je element 1 potem je -)
-       ce tak razcep obstaja(npr. [1,2,0,3,1] pri bazi
-       [2,3,5,7] pomeni da se to stevilo
-       zapise kot -2**2*3**0*5**3*7), sicer None
-    """
-
-    factors = [0]*len(base)
-    num1 = NumberMod(num,mod)
-    Prime = num1.isPrime()
-    #ce je stevilo prastevilo pogledamo
-    #ce lahko razcepimo stevilo - mod
-    if Prime and num1.num not in base:
-        num1 = NumberMod(-num,mod)
-        
-    stevilo = num1.num
-    st = 0
-    for el in base:
-        while stevilo % el == 0:
-            factors[st] += 1
-            stevilo  = stevilo // el
-        st += 1
-
-    if Prime and num1.num not in base and stevilo == 1:
-        odg = [1] + factors
-    else:
-        odg = [0] + factors
-
-    if (odg != [1] +[0]*len(base) and
-        odg != [0]*(len(base)+1) and
-        stevilo == 1):
-        return odg
-    else:
-        return None
-    
-def FindSystemMod(mod,g,base):
-    """
-    Opis:
-       FindSystemMod  vrne  sistem enacb, ki jih moramo
-       resiti, da resimo problem diksretnega logaritma.
-
-     Definicija:
-       FindSystemMod(mod,g,base)
-
-     Vhodni podatki:
-       g...generator multiplikativne grupe $\Z_mod$.
-       mod...modul s katerim delamo
-       base...baza prastevil s katerimi delamo
-
-     Izhodni  podatek:
-       par (A,b)...A je matrika sistema,
-                   b je desna stran sistema
-    """
-    #kratnik doloca koliko enacb je v sistemu
-    #(n pomeni dolzina_baze*n enacb)
-    kratnik = 5
-    #pove ali dodamo enacbo v sistem ali ne
-    dodamo = False 
-    #pove ali ze imamo pokrite vse spremenljivke
-    #z dosedanjimi enacbami
-    VSE = False
-    #porazdelitev spremenljivk v posameznih
-    #enacbah ki smo jih dodali
-    mamo = [[0]*len(base) for i in range(kratnik*len(base))]
-    #katere spremenljivke smo ze pokrili
-    mamoSPR = [0]*len(base)
-    st = 0#stevilo dodanih enacb
-    A = []
-    b = []
-    for i in range(1,mod-1):
-        #razcepimo stevilo na faktorje iz baze
-        odg = PrimeSearch(g**i,mod,base)
-        if odg != None:
-            #pogledamo ce smo dobili kaksno
-            #novo spremenljivko
-            if VSE == False:
-                for j in range(1,len(base)+1):
-                    #zamik zarad +- 1 spredi
-                    if odg[j] != 0 and mamoSPR[j-1] == 0:
-                        #ce se nimamo pokrite te
-                        #spremenljivke to enacbo
-                        #vzamemo
-                        mamoSPR[j-1] = 1
-                        dodamo = True
-                        
-            elif VSE and len(A) < kratnik*len(base):
-                #nimamo se dovolj velikega sistema
-                #preverimo ce tako enacbo ze mamo
-                dodamo = True
-                tmp = list(map(bool,odg[1:]))
-                for k in range(st):
-                    if mamo[k] == tmp:
-                        dodamo = False
-            if dodamo:
-                mamo[st] = list(map(bool,odg[1:]))
-                st += 1
-                A.append(odg[1:])
-                b.append(i-((mod-1)//2)*odg[0])
-                dodamo = False
-                if np.prod(mamoSPR) == 1:
-                    VSE = True
-            
-
-        if (np.prod(mamoSPR) != 0 and
-            len(A) >= kratnik*len(base)):
-            #prekinemo iskanje ce imamo dovolj enacb
-            break
-
-    
-    return(A,b)
-        
-        
-
-def SolveSystemMod(A,b,mod):
-    """
-Opis:
-   SolveSystemMod  vrne  resitev sistema linearnih enacb
-   podanih z matrikama A in b (Ax = b) modulo stevilo mod.
-   Metoda za resevanje sistem pretvori v novo obliko tako,
-   da vsako vrstico pretvori v novo enacbo in potem
-   celotni sistem resi s pomocjo celostevilskega linearnega
-   programa iz paketa mip
-
-   Zgled:
-       A = [[0	,1	,0	,0	,0	,0	],
-            [2	,0	,0	,1	,0	,1	],
-            [0	,0	,3	,0	,0	,0	],
-            [1	,0	,2	,0	,0	,0	],
-            [0	,0	,1	,0	,1	,0	],
-            [0	,0	,0	,0	,0	,1	]]
-       b = [1,-584,25,-578,-554,87]
-
-       mod = 1216
-
-       posamezno vrstico pretvori tako, da namesto x2 = 1
-       dobimo vrstico x2 + mod*X1(dodatna spremenljivka) = 1
-       (za vsako vrstico uvedemo novo dodatno spremenljivko)
-
- Definicija:
-   SolveSystemMod(A,b,mod)
-
- Vhodni podatki:
-   A...matrika podana kot [[...],[...],[...],...,[...]]
-   b...desna stran sistema podana s seznamom
-   mod...modulo glede na katerega resujemo sistem
-
- Izhodni  podatek:
-   x  seznam vrednosti resitve sistema
-"""
-    n =len(A)
-    dol_vrstice = len(A[0])
-    Kopija = A[:]
-    #pretvorimo matriko A v primerno obliko
-    for i in range(n):
-        tmp = [0]*n
-        tmp[i] = mod
-        Kopija[i] = Kopija[i] + tmp
-
-    #naredimo linearni program(zanimajo nas samo
-    #spremenljivke, ki predstavljajo bazo,
-    #katere omejimo med 0 in mod-1)
-    m = mip.Model()
-    x = [ m.add_var(var_type=mip.INTEGER,lb=1,ub = mod-1)
-          for i in range(dol_vrstice) ]
-    #spremenljivk tok kukr jih je v vrstici
-    for i in range(n):
-        x.append(m.add_var(var_type=mip.INTEGER,lb = -10,ub = 10))
-        #dodatna spremenljivka za vsako vrstico posebaj
-    m.objective = mip.minimize(
-        mip.xsum(0*x[j] for j in range(dol_vrstice)))
-    for i in range(n):
-        m += mip.xsum(
-            Kopija[i][j]*x[j] for j in range(dol_vrstice + n)) == b[i]
-    m.optimize()
-
-    #zaokrozimo za lepsi izpis(vcasih
-    #lahko pride resitev 0.999999999 namesto 1, kar je isto)
-    resitev = [int(round(x[i].x)) for i in range(dol_vrstice)]
-    
-    return(resitev)
-
-
-def IndexCalculus(g,mod,a,base):
+def IndexCalculus(g,h,q,baza):
     """
     Opis:
        IndexCalculus  vrne  tak $k$, da velja
-       $$g^k \equiv a (mod mod)$$, kjer je g generator
-       multiplikativne grupe $\Z_mod$.
+       $$g^k \equiv h (mod q)$$, kjer je g generator
+       multiplikativne grupe $\Z_q$.
 
        Zgled:
            g = 3
-           mod = 1217
-           a = 37
-           base = [2,3,5,7,11,13]
+           q = 1217
+           h = 37
+           baza = [-1,2,3,5,7,11,13]
 
      Definicija:
-       IndexCalculus(g,mod,a,base)
+       IndexCalculus(g,h,q,baza)
 
      Vhodni podatki:
-       g...generator multiplikativne grupe $\Z_mod$.
-       mod...modul s katerim delamo
-       a...desna stran problema, ki ga resujemo
-       base...baza prastevil s katerimi delamo
+       g...generator multiplikativne grupe $\Z_q$.
+       q...modul s katerim delamo
+       h...desna stran problema, ki ga resujemo
+       baza...baza prastevil s katerimi delamo more
+               vsebovati tudi -1
 
      Izhodni  podatek:
-       $k$, da velja $$g^k \equiv a (mod mod)$$
+       $k$, da velja $$g^k \equiv h (mod q)$$
     """
-    Temp = FindSystemMod(mod,g,base)
-    A = Temp[0]
-    b = Temp[1]
-    resitev = SolveSystemMod(A,b,mod-1)
-    k = 0
+    
+    seznam_relacij = []
+    V = VectorSpace(GF(q),len(baza)+1)
+    Z = GF(q)
+    k = 1
+    while True:
+        e0 = 0
+        st = g^k % q
+        if is_prime(st) and st not in baza:
+            st = abs(st-q)
+            e0 = 1
+        tmp = ecm.factor(st)
+        if set(tmp).issubset(set(baza)):
+            relacija = [0]*(len(baza)+1)
+            relacija[0] = e0
+            for i in range(1,len(baza)):
+                relacija[i] = tmp.count(baza[i])
+            relacija[-1] = k
 
-    for i in range(1,mod-1):
-        faktorji = PrimeSearch((g**i)*a,mod,base)
-        if  faktorji != None:
-            k = sum([a*b for a,b in zip(faktorji[1:],resitev)])
-            k+=faktorji[0]*((mod-1)//2) -i 
-            return NumberMod(k,mod-1).num
+            seznam_relacij.append(relacija)
+            M = Matrix(Z,seznam_relacij)
+            if len(V.linear_dependence(M)) >0:
+                seznam_relacij = seznam_relacij[:-1]
 
-
-#primer
-base = [2,3,5,7,11,13]
-g = 27
-mod = 1217
-k = IndexCalculus(g,mod,37,base)
+            if len(seznam_relacij) > len(baza):
+                break
+        k+=1
+            
+    tmp = seznam_relacij[:]
+    b = [0]*(len(baza)+1)
+    for i in range(len(baza)+1):
+        if seznam_relacij[i][0] == 1:
+            b[i] = seznam_relacij[i][-1] - ((q-1)//2)
+        else:
+            b[i] = seznam_relacij[i][-1]
+        tmp[i] = tmp[i][1:-1]
+    
+    
+    M = Matrix(IntegerModRing(q-1),tmp)
+    B = vector(IntegerModRing(q-1),b)
+    X = M.solve_right(B)
+    print(X)
+    
+    j = 0
+    while True:
+        rez = (power_mod(g,j,q)*h) % q
+        tmp = ecm.factor(rez)
+        if set(tmp).issubset(set(baza)):
+            enacba = [0]*(len(baza)-1)
+            for i in range(1,len(baza)):
+                enacba[i-1] = tmp.count(baza[i])
+            enacba = vector(IntegerModRing(q-1),enacba)
+            
+            return( enacba*X - j)
+        j+=1
--- magistersko delo/MOV.py
+++ magistersko delo/MOV.py
@@ -1,364 +1,60 @@
-import math
-import random
-import numpy as np
-from base import *
-
-
-    
-def BabyGiant(P):
+def MOV(E,q,m,P,Q,st_korakov = 10):
     """
     Opis:
-       Funkcija BabyGiant je implementacija algoritma
-       Baby step, Giant step za iskanje reda tocke P
+       Funkcija MOV resuje problem diskretnega logaritma
+       na krivulji E, podanega s tockama P,Q.
+       Iscemo torej $k$, tako da velja $kP = Q$.
 
      Definicija:
-       BabyGiant(P)
+       MOV(E,q,m,P,Q,st_korakov = 10)
 
      Vhodni podatki:
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
+       E...Elipticna krivulja iz SAGE
+       q...modul krivulje
+       m...stevilo ki pove razsiritev prvotnega polja
+           iz GF(q) gremo v GF(q^m) (GF = Galvajevo polje)
+       P...tocka na elipticni krivulji iz SAGE
+       Q...tocka na elipticni krivulji iz SAGE
+       st_korakov...v algoritmu dobivamo rezultate po nekem
+                    modolu in moramo na koncu s pomocjo
+                    kitajskega izreka dobit koncni rezultat.
+                    Lahko pa se zgodi da vedno dobivamo po
+                    istih modulih in bi se lahko zaciklali.
+                    st_korakov omeji kolikokrat se ta del
+                    zanke izvede.
 
      Izhodni  podatek:
-       int k, ki predstavlja red tocke P (kP = $\infty$)
+       stevilo k
     """
-    q = P.mod
-    Q = (q+1)*P
-    m = int(q**(1/4))+1
-    seznam = []
-
-    for j in range(m+1):
-        seznam.append(j*P)
-
-
-    tmp = (2*m)*P
-    for k in range(-m,m+1):
-        T = Q + k*tmp
-        if T in seznam:
-            j = seznam.index(T)
-            break
-        elif -1*T in seznam:
-            j = seznam.index(-1*T)
-            break
-    st1 = q+1+2*m*k -j
-    st2 = q+1+2*m*k +j
-    if st1*P == INFPoint:      
-        M = st1
-    else:
-        M = st2
-
-    odg = pomozna(M,P)
-
-    return odg
-
-
-def BabyGiantCount(E):
-    q = E.mod
-    mejasp = q+1-2*(q**0.5)
-    mejazg = q+1+2*(q**0.5)
-    seznamst = [i for i in range(int(mejasp),int(mejazg+1))]
-    redi = []
+    N = P.order()
+    k = GF(q^m,'x')
+    E1 = E.base_extend(k)#razsirimo krivuljo na vecje polje
+    P1 = E1(P)#razsirit moramo tudi tocki
+    Q1 = E1(Q)
     
-    #vsaj en red rabmo
-    T = E.rand()
-    red = BabyGiant(T)
-    redi.append(red)
-    lcm = red
-
+    odgovori = [[],[]]
+    koraki = 0
+    
     while True:
-        T = E.rand()
-        red = BabyGiant(T)
-        redi.append(red)
-        lcm = lcm*red/gcd(lcm,red)
-        st = 0
-        for el in seznamst:
-            if el%lcm == 0 and st ==0:
-                N = el
-                st += 1
-            elif el%lcm == 0 and st >0:
-                st += 1
-                break
-
-        if st == 1:
-            return N
-    
-    
-
-
-def pomozna(M,P):
-    """
-    Opis:
-       Funkcija pomozna predstavlja rekurzivni del funkcije
-       BabyGiant
-
-     Definicija:
-       pomozna(M,P)
-
-     Vhodni podatki:
-       M...stevilo, ki ga testiramo ce predstavlja red
-           tocke P
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-
-     Izhodni  podatek:
-       int k, ki predstavlja red tocke P (kP = $\infty$)
-    """
-    faktorji = Factor(M)
-    for el in faktorji:
-        if (M//el)*P == INFPoint:
-            rez = pomozna(M//el,P)
-            return rez
-        else:
-            return M
-
-
-def naklon(P,Q):
-    """
-    Opis:
-       Funkcija naklon izracuna naklon premice med tockama
-       P in Q, ki lezita na elipticni krivulji
-
-     Definicija:
-       naklon(P,Q)
-
-     Vhodni podatki:
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       Q...razred Point, ki predstavlja tocko na
-           elipticni krivulji   
-
-     Izhodni  podatek:
-       stevilo po modolu P.mod, ki predstavlja naklon
-    """
-    mod = P.mod
-    if P == Q:
-        st = (3*(P.x**2)+P.a) % mod
-        im = NumberMod(2*(P.y),mod).inverse().num
-        rez = (st*im) % mod
-        return rez
-    else:
-        st = (Q.y-P.y) % mod
-        im = NumberMod(Q.x-P.x,P.mod).inverse().num
-        rez = (st*im) % mod
-        return rez
-
-
-
-def g(P,Q,X):
-    """
-    Opis:
-       Funkcija g del funkcije potrebne za izracun Weilovega
-       parjenja, s pomocjo Millerjevega algoritma. Funkcija izracuna
-       funkcijo $g_{P,Q}(X)$
-
-     Definicija:
-       g(P,Q,X)
-
-     Vhodni podatki:
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       Q...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       X...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-
-     Izhodni  podatek:
-       stevilo, ki predtavlja vrednost $g_{P,Q}(X)$
-    """
-    if P.x == Q.x and P.y != Q.y:
-        rez = (X.x-P.x) % P.mod
-        return rez
-    else:
-        #lambda je naklon premice
-        lam = naklon(P,Q)
-        st = (X.y-P.y-lam*(X.x-P.x)) % P.mod
-        im = NumberMod(X.x+P.x+Q.x-lam**2,P.mod).inverse().num
-        rez = (st*im) % P.mod
-        return rez
-
-def Miller(P,X,m):
-    """
-    Opis:
-       Funkcija Miller je implementacija Millerjevega algoritma
-       potrebnega za izracun Weilovega parjenja. Ce je
-       $$e_m(P,Q) = (f_P(Q+S)/f_P(S)) / (f_Q(P-S)/f_Q(-S)) $$
-       potem funkcija Miller predstavlja izracun vrednosti
-       $f_P(X)$.
-
-     Definicija:
-       Miller(P,X,m)
-
-     Vhodni podatki:
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       X...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       m...red tocke P
-
-     Izhodni  podatek:
-       vrednost funkcije $f_P(X)$
-    """
-    binarno = bin(m)[2:] #vrne niz porezemo 0b na zacetku niza
-    n = len(binarno)
-    T = P
-    f = 1
-    for i in range(1,n):
-        f = (f**2 * g(T,T,X)) % P.mod
-        T = 2*T
-        if int(binarno[i]) == 1:
-            f = (f* g(T,P,X)) % P.mod
-            T = T + P
-    return f
-
-
-def WeilPairing(P,Q,S,N):
-    """
-    Opis:
-       Funkcija WeilPairing je implementacija Weilovega
-       parjenja $e_N(P,Q)$, kjer je
-       $$e_N(P,Q) = (f_P(Q+S)/f_P(S)) / (f_Q(P-S)/f_Q(-S)) $$
-
-     Definicija:
-       WeilPairing(P,Q,S,N)
-
-     Vhodni podatki:
-       P...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       Q...razred Point, ki predstavlja tocko na
-           elipticni krivulji
-       S...razred Point, ki predstavlja tocko, ki
-           ni v podgrupi generirani z P,Q
-       N...red tocke P
-
-     Izhodni  podatek:
-       int k, ki predstavlja red tocke P (kP = $\infty$)
-    """
-    
-    fpQS = Miller(P,Q+S,N)
-    fpS = Miller(P,S,N)
-    fqPS = Miller(Q,P-S,N)
-    fqS = Miller(Q,-S,N)
-
-    fpS = NumberMod(fpS,P.mod).inverse().num
-    eN1 = (fpQS * fpS) % P.mod
-    
-    fqS = NumberMod(fqS,P.mod).inverse().num
-    eN2 = (fqPS * fqS) % P.mod
-    eN2 = NumberMod(eN2,P.mod).inverse().num
-
-    eN  = (eN1*eN2) % P.mod
-
-    return eN
-    
-
-P = Point(30,34,631,617,5)
-Q = Point(30,34,631,121,244)
-S = Point(30,34,631,0,36)
-
-eN = WeilPairing(P,Q,S,5)
-    
-    
-
-
-def MOV(E,m,N,P,Q):
-    E1 = ElipticCurve(E.a,E.b,E.mod**m)
-    while True:
-        print("tuki")
-        T = E1.rand()
-        red = BabyGiant(T)
-        d = gcd(red,N)
-        print(d)
-        T1 = (red//d)*T
-        print(T1)
-        print(BabyGiant(T1))
-        while True:
-            S = E1.rand()
-            red1 = BabyGiant(S)
-            print(red1)
-            if red1 > red:
-                break
-        zeta1 = WeilPairing(P,T1,S,N)
-        zeta2 = WeilPairing(Q,T1,S,N)
-        break
-
-    return (zeta1,zeta2)
-
-
-def projektivnaAdd(P,Q):
-    x1 = P.x
-    x2 = Q.x
-    y1 = P.y
-    y2 = Q.y
-    z1 = 1
-    z2 = 1
-    a = P.a
-    b = P.b
-    mod = P.mod
-    x31 = (x1*y2-x2*y1)*(y1*z2+y2*z1)+ (x1*z2-x2*z1)*y1*y2 \
-            -a*(x1*z2 + x2*z1)*(x1*z2-x2*z1)-3*b*(x1*z2-x2*z1)*z1*z2
-    y31 = -3*x1*x2*(x1*y2-x2*y1)-y1*y2*(y1*z2-y2*z1)-a*(x1*y2-x2*y1)*z1*z2 \
-          + a*(x1*z2+x2*z1)*(y1*z2-y2*z1)+3*b*(y1*z2-y2*z1)*z1*z2
-    z31 = 3*x1*x2*(x1*z2-x2*z1)-(y1*z2+y2*z1)*(y1*z2-y2*z1) \
-          + a*(x1*z2-x2*z1)*z1*z2
-
-    x31 = x31 % mod
-    y31 = y31 % mod
-    z31 = z31 % mod
-
-    x32 = y1*y2*(x1*y2+x2*y1)-a*x1*x2*(y1*z2+y2*z1)-a*(x1*y2+x2*y1)*(x1*z2+x2*z1) \
-          -3*b*(x1*y2+x2*y1)*z1*z2 - 3*b*(x1*z2+x2*z1)*(y1*z2+y2*z1)\
-          + a*a*(y1*z2+y2*z1)*z1*z2
-    y32 = y1*y1*y2*y2 + 3*a*x1*x1*x2*x2+9*b*x1*x2*(x1*z2+x2*z1)\
-          -a*a*x1*z2*(x1*z2+2*x2*z1)-a*a*x2*z1*(2*x1*z2+x2*z1)\
-          -3*a*b*z1*z2*(x1*z2+x2*z1)-(a*a*a+9*b*b)*z1*z1*z2*z2
-    z32 = 3*x1*x2*(x1*y2+x2*y1)+y1*y2*(y1*z2+y2*z1)+a*(x1*y2+x2*y1)*z1*z2\
-          +a*(x1*z2+x2*z1)*(y1*z2+y2*z1)+3*b*(y1*z2+y2*z1)*z1*z2
-
-    x32 = x32 % mod
-    y32 = y32 % mod
-    z32 = z32 % mod
-
-    x33 = (x1*y2+x2*y1)*(x1*y2-x2*y1) + a*x1*x2*(x1*z2-x2*z1)\
-          +3*b*(x1*z2+x2*z1)*(x1*z2-x2*z1)-a*a*(x1*z2-x2*z1)*z1*z2
-    y33 = (x1*y2-x2*y1)*y1*y2-3*a*x1*x2*(y1*z2-y2*z1)\
-          +a*(x1*y2+x2*y1)*(x1*z2-x2*z1)+3*b*(x1*y2-x2*y1)*z1*z2\
-          -3*b*(x1*z2+x2*z1)*(y1*z2-y2*z1)+a*a*(y1*z2-y2*z1*z1*z2)
-    z33 = -(x1*y2+x2*y1)*(y1*z2-y2*z1)-(x1*z2-x2*z1)*y1*y2 \
-          -a*(x1*z2+x2*z1)*(x1*z2-x2*z1)-3*b*(x1*z2-x2*z1)*z1*z2
-
-    x33 = x33 % mod
-    y33 = y33 % mod
-    z33 = z33 % mod
-
-    tocka = ((x31+x32+x33)%mod,(y31+y32+y33)%mod,(z31+z32+z33)%mod)
-    print([[x31,y31,z31],[x32,y32,z32],[x33,y33,z33]])
-
-    try:
-        inv = NumberMod(tocka[2],mod).inverse().num
-        tocka = (tocka[0]*inv %mod,tocka[1]*inv% mod,1)
-    except:
-        pass
- 
-
-    #return [[x31,y31,z31],[x32,y32,z32],[x33,y33,z33]]
-    return tocka
-
-##E = ElipticCurve(1,1,13)
-##P = E.rand()
-##Q = E.rand()
-
-P = Point(-1,1,25,1,1)
-Q = Point(-1,1,25,21,4)#k = 5
-        
-
-E = ElipticCurve(0,1,23)
-P = Point(0,1,23,12,2)
-m = 2
-N = 24
-Q = Point(0,1,23,1,18)
-l = 12
-E1 = ElipticCurve(0,1,23**m)
-##a = MOV(E,m,N,P,Q)
-a = Point(0,1,529,253,1)
-    
-##test = BabyGiant(Point(-10,21,557,2,3))
-##print(test)
+        #vsak ta del nam da odgovor po modolu d
+        T = E1.random_element()
+        M = T.order()
+        d = gcd(N,M)
+        T1 = int(M/d)*T
+        w1 = P1.weil_pairing(T1,N)
+        print(w1)
+        w2 = Q1.weil_pairing(T1,N)
+        print(w2)
+        if w1 != w2:
+            #k = Naivno(w1,w2)
+            k = discrete_log(w2, w1)
+            odgovori[0].append(k)
+            odgovori[1].append(d)
+        if lcm(odgovori[1]) >= q:
+            break
+        elif koraki > st_korakov:
+            break
+        koraki+=1
+    #uporabimo kitajski izrek o ostankih
+    k = crt(odgovori[0],odgovori[1])
+    return k
